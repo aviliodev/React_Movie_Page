f47151f97de4947b3f5eb2d5355ae99748972dd5
aviliodev <quidralion@gmail.com>
2021-05-17 16:57:44 -0600

    5. Se agregaron hojas de estilo faltantes y se empezó a agregar los hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -6,35 +6,46 @@ import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
+import { useEffect, useState } from 'react'
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
-        <Categories>
-            <Carousel>
-                <CarouselItem /> 
-                <CarouselItem />    
-                <CarouselItem /> 
-                <CarouselItem />             
-            </Carousel>
-        </Categories>
+const App = () => {
+    const [videos, setVideos] = useState([]) /*creación de dos constantes que van a ser utilizadas por las funciones de abajo*/
 
-        <Categories>
-            <Carousel>
-                <CarouselItem /> 
-                <CarouselItem />               
-            </Carousel>
-        </Categories>
+    useEffect( () => {
+        fetch(API)
+            .then(response => response.json())
+            .then(data => setVideos(data))
+    }, [])
 
-        <Categories>
-            <Carousel>
-                <CarouselItem />             
-            </Carousel>
-        </Categories>
-        <Footer/>
+    return (
+            <div className="App">
+                <Header />
+                <Search />
+                <Categories>
+                    <Carousel>
+                        <CarouselItem /> 
+                        <CarouselItem />    
+                        <CarouselItem /> 
+                        <CarouselItem />             
+                    </Carousel>
+                </Categories>
 
-    </div>
-)
+                <Categories>
+                    <Carousel>
+                        <CarouselItem /> 
+                        <CarouselItem />               
+                    </Carousel>
+                </Categories>
+
+                <Categories>
+                    <Carousel>
+                        <CarouselItem />             
+                    </Carousel>
+                </Categories>
+                <Footer/>
+
+            </div>
+        )
+}
 
 export default App
